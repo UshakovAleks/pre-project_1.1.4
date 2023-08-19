df8cf42d95544b2702ca6deeee40e2d9308b9a18
UshakovAleks <vladimirkulikov555@mail.ru>
2023-08-19 18:31:58 +0300

    1.1.5 Added SesionFactory

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,14 +7,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Util {
 
     private Connection connection;
-
+    private SessionFactory sessionFactory;
     private static Util instance;
     private Util() {
         this.connection = getCon();
+        this.sessionFactory = getSession();
     }
     public static Util getUtil() {
         if (instance == null) {
@@ -26,7 +29,7 @@ public class Util {
         return connection;
     }
 
-        private static Connection getCon() {
+    private static Connection getCon() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(
@@ -36,8 +39,6 @@ public class Util {
         }
         return connection;
     }
-
-
 
     private static String getConfig(String config) {
         FileInputStream fis;
@@ -54,4 +55,24 @@ public class Util {
         }
         return conf;
     }
+
+    private static SessionFactory getSession(){
+        Properties prop= new Properties();
+
+        prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/test");
+
+        prop.setProperty("dialect", "org.hibernate.dialect.MySQL8Dialect");
+
+        prop.setProperty("hibernate.connection.username", "root");
+        prop.setProperty("hibernate.connection.password", "Valera83797");
+        prop.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        prop.setProperty("show_sql", "true");
+
+        SessionFactory sessionFactory = new Configuration().addProperties(prop).buildSessionFactory();
+        return  sessionFactory;
+    }
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
 }
